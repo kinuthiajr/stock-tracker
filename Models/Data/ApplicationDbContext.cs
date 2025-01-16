fa27373aee0d13afad4f599e47d54cddfe2b0ab9
smk <kinuthiajr@gmail.com>
2025-01-16 11:18:00 -0800

    Configured IdentityUser

--- Models/Data/ApplicationDbContext.cs
+++ Models/Data/ApplicationDbContext.cs
@@ -2,13 +2,14 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace api.Models.Data
 {
-    public class ApplicationDbContext(DbContextOptions dbContextOptions) : DbContext(dbContextOptions)
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<AppUser>(options)
     {
-        public required DbSet<Stock> Stock {get;set;}
-        public required DbSet<Comment> Comments{get;set;}
+        public DbSet<Stock>? Stock { get; set; }
+        public DbSet<Comment>? Comments { get; set; }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,7 +1,11 @@
 using api.Interfaces;
+using api.Models;
 using api.Models.Data;
 using api.Repository;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -21,6 +25,37 @@ builder.Services.AddDbContext<ApplicationDbContext> (options => {
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 }); // Added this after making the Data folder so as to connect with sqlite
 
+builder.Services.AddIdentity<AppUser, IdentityRole>(options => {
+    options.Password.RequireDigit = true;
+    options.Password.RequireLowercase = true;
+    options.Password.RequireUppercase =true;
+    options.Password.RequireNonAlphanumeric =true;
+    options.Password.RequiredLength = 17;
+}).AddEntityFrameworkStores<ApplicationDbContext>();
+
+builder.Services.AddAuthentication(options => {
+
+    options.DefaultAuthenticateScheme = 
+    options.DefaultChallengeScheme =
+    options.DefaultForbidScheme =
+    options.DefaultScheme =
+    options.DefaultSignInScheme =
+    options.DefaultSignOutScheme = JwtBearerDefaults.AuthenticationScheme;
+
+}).AddJwtBearer(options => {
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidIssuer = builder.Configuration["JWT:Issuer"],
+        ValidateAudience = true,
+        ValidAudience = builder.Configuration["JWT:Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(
+            System.Text.Encoding.UTF8.GetBytes(builder.Configuration["JWT:SigningKey"])
+        )
+    };
+
+});
+
 builder.Services.AddScoped<IStockRepository, StockRepository>(); // Registered this Interface&Repo
 builder.Services.AddScoped<ICommentRepository, CommentRepository>(); // Registered this Interface&Repo
 
@@ -34,6 +69,9 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.MapControllers(); // Added 
 app.Run();
